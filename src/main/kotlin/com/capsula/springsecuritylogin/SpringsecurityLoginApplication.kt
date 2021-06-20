dd760c6a9d01ddf43bb8f25cfa15bb2c8c75cebf
Androide <jchocobar886@gmail.com>
2021-06-20 19:25:54 -0300

    Initial commit

---
+++ src/main/kotlin/com/capsula/springsecuritylogin/SpringsecurityLoginApplication.kt
@@ -0,0 +1,13 @@
+package com.capsula.springsecuritylogin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringsecurityLoginApplication{
+	
+}
+
+fun main(args: Array<String>) {
+	runApplication<SpringsecurityLoginApplication>(*args)
+}
---
+++ src/test/kotlin/com/capsula/springsecuritylogin/SpringsecurityLoginApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.capsula.springsecuritylogin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringsecurityLoginApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
